d734c6fc5ab94cc89961d960723da76f6df54f5e
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-05-21 17:29:30 +0200

    Fix extension

--- sources/parseXmlSccm.ps1
+++ sources/parseXmlSccm.ps1
@@ -1,2 +1,2 @@
 cd \\s-sccm-01\r$\Repository\SourceIn\Applications
-Get-ChildItem -Recurse $pwd | Where-Object {$_.Extension -ilike ".xml"} | Foreach-Object {$_.FullName} | > %TEMP%\ressources.txt
+Get-ChildItem -Recurse $pwd | Where-Object {$_.Extension -ilike "Package.xml"} | Foreach-Object {$_.FullName} | > %TEMP%\ressources.txt
